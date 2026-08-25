7ed56327b62fe129bb691e7f8398b9ba13244b2a
arkamedha <pankajarka01@gmail.com>
2026-08-26 00:52:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// Firebase को इंटरनेट (CDN) से इम्पोर्ट कर रहे हैं
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
-// आपकी Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBUG6LDNhB8kPW4fh8XURtIBb8ZpPNsM4w",
   authDomain: "arka-medha-test-app.firebaseapp.com",
@@ -12,22 +10,18 @@ const firebaseConfig = {
   appId: "1:249649360234:web:3fcd302e5115573caa8913"
 };
 
-// Firebase शुरू करें
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// HTML के बटन और इनपुट बॉक्स को जावास्क्रिप्ट से जोड़ना
 const loginBtn = document.getElementById("loginBtn");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const messageDisplay = document.getElementById("message");
 
-// जब Login बटन पर क्लिक हो
 loginBtn.addEventListener("click", () => {
     const email = emailInput.value;
     const password = passwordInput.value;
 
-    // चेक करें कि ईमेल और पासवर्ड खाली तो नहीं है
     if(email === "" || password === "") {
         messageDisplay.innerText = "Please enter both Email and Password!";
         messageDisplay.style.color = "red";
@@ -37,19 +31,14 @@ loginBtn.addEventListener("click", () => {
     messageDisplay.innerText = "Logging in please wait...";
     messageDisplay.style.color = "blue";
 
-    // Firebase से लॉगिन करने का कोड
     signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            const user = userCredential.user;
             messageDisplay.innerText = "Login Successful! ✅";
-            window.location.href = "dashboard.html";
             messageDisplay.style.color = "green";
-            console.log("Logged in user:", user.email);
-            // अगले स्टेप में हम यहाँ से यूज़र को 'Test Dashboard' पर भेजेंगे
+            window.location.href = "dashboard.html";
         })
         .catch((error) => {
             messageDisplay.innerText = "Error: Invalid Email or Password ❌";
             messageDisplay.style.color = "red";
-            console.error(error.message);
         });
 });
